c1937aa2b940928cf724a8b8ae35a17c4e6af450
Dev Agrawal <devagrawal09@gmail.com>
2021-09-09 14:40:27 -0400

    Added javadocs

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,14 +3,27 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Handle the index and start endpoints
+ * @author Dev Agrawal
+ * @return
+ */
 @Controller
 public class PlantPlacesController {
 	
+	/**
+	 * Handle the index endpoint
+	 * @return
+	 */
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
 
+	/**
+	 * Handle the start endpoint
+	 * @return
+	 */
 	@RequestMapping("/start")
 	public String start() {
 		
